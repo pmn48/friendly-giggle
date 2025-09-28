fefb1cb9cdc6ba7297683496a46575c4aaeb0bfa
pmn48 <138720690+pmn48@users.noreply.github.com>
2025-09-28 03:37:29 -0400

    Time: 0 ms (100.00%) | Memory: 43.1 MB (51.75%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -7,17 +7,10 @@ class Solution { // BINARY SEARCH ALGO
 
     private int binarySearch(int[] nums, int target, int lo, int hi) {
 
-        // invalid case: this is when target is greater than all elements in num, pushing mid up
-        // to the point where mid will be greater than hi
-        // append target to the end index, at this point is high
+        // invalid case: this is when target is greater than all elements in nums, pushing mid up
+        // to the point where (mid + 1) > hi
+        // OR smaller than all elements in nums, pushing mid down to the point where lo > (mid - 1)
         if (lo > hi) return lo; 
-
-        // this is when the target is smaller than all elements in num, pushing mid equal to lo
-        if (lo == hi) {
-            if (target <= nums[hi]) return hi;
-            else if (target > nums[hi]) return hi + 1; 
-            // everything to the left should be smaller than target
-        }
 
         int mid = (lo + hi)/2;
 
@@ -27,7 +20,7 @@ class Solution { // BINARY SEARCH ALGO
             return binarySearch(nums, target, mid + 1, hi);
         }
         else { // (target < nums[mid])
-            return binarySearch(nums, target, lo, mid);
+            return binarySearch(nums, target, lo, mid - 1);
         }
         
     }
@@ -35,8 +28,8 @@ class Solution { // BINARY SEARCH ALGO
     // O(log n): operation that divides the input by a constant factor (2). 
     // In each iteration, it is an O(1) operation
     // Dividing the input arrays into half, left and right by mid = (lo + hi) / 2 until A[mid] = target
-    // left array: everything smaller or equal to target (lo to mid)
+    // left array: everything smaller than target (lo to mid - 1)
     // right right: everything that greater than target (mid + 1 to hi)
-    // if target in array: it will be either the last one in left (mid)
+    // if target in array: it will nums[mid]
     // else: insert it to the position after mid 
 }
