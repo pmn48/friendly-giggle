8171acf29961b9d7292dc0ae13697bbc01ca9336
pmn48 <138720690+pmn48@users.noreply.github.com>
2025-10-03 22:29:26 -0400

    Time: 8 ms (84.73%) | Memory: 47.3 MB (6.25%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -5,6 +5,35 @@ class Solution {
         // sort all elements by start time (O(n))
         Arrays.sort(intervals, (a , b) -> Integer.compare(a[0], b[0]));
 
+        // keep track of the current element in interval at merge_index
+        int int_index = 1;
+        int merge_index = 0;
+        while (int_index < intervals.length) {
+            // if end time of cur >= start time of element in interval
+            if (intervals[merge_index][1] >= intervals[int_index][0]) {
+                intervals[merge_index][1] = Math.max(intervals[merge_index][1], intervals[int_index][1]);
+            } else { //there is no overlap 
+                merge_index++; // move forward and copy to next
+                intervals[merge_index] = intervals[int_index];
+
+            }
+            int_index++;
+        }
+
+        // only until the updated interval
+        return Arrays.copyOfRange(intervals, 0, merge_index + 1);
+    }
+
+}
+
+// Separate array
+/*
+public int[][] merge(int[][] intervals) {
+        if (intervals.length <= 1 || intervals == null) return intervals;
+        
+        // sort all elements by start time (O(n))
+        Arrays.sort(intervals, (a , b) -> Integer.compare(a[0], b[0]));
+
         // create an array to hold the merged intervals
         List<int[]> res_list = new ArrayList<>();
 
@@ -17,7 +46,8 @@ class Solution {
             if (cur[1] >= intervals[int_index][0]) {
                 cur[1] = Math.max(cur[1], intervals[int_index][1]); // new end time
                 // do not add cur yet until it is checked against the next interval
-            } else { // else, there is no overlap so add cur to list before updating to cur
+            } else { //there is no overlap so add cur to list before updating to cur
+
                 res_list.add(cur);
                 cur = intervals[int_index];   
             }
@@ -30,6 +60,4 @@ class Solution {
         res_list.add(cur);
 
         return res_list.toArray(new int[res_list.size()][]);
-    }
-
-}
+}*/
