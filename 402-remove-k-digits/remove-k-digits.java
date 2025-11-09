2ab0e0ee332ea351faa8f4aca1b260fc45803050
pmn48 <138720690+pmn48@users.noreply.github.com>
2025-11-08 22:09:01 -0500

    Time: 31 ms (15.53%) | Memory: 46.5 MB (13.92%) - LeetSync

---
+++ 402-remove-k-digits/remove-k-digits.java
@@ -0,0 +1,61 @@
+class Solution {
+    public static String stackToStr(Stack<Character> myStack) {
+        StringBuilder strB = new StringBuilder();
+
+        while (!myStack.empty()) {
+            char charNum = myStack.pop();
+            strB.append(charNum);
+        }
+
+        // reverse string to get final answer
+        strB.reverse();
+
+        // convert to string
+        String resStr = strB.toString();
+
+        // get rid of leading 0
+        int i = 0;
+        while (resStr.length() > i && resStr.charAt(i) == '0') {
+            i++;
+        }
+
+        return resStr.substring(i);
+    }
+
+    public String removeKdigits(String num, int k) {
+        Stack<Character> myStack = new Stack<>();
+
+        for (char charInNum: num.toCharArray()) {
+           
+            // keep removing the top number if it is greater than the current number in num
+            while (!myStack.empty() && myStack.peek() > charInNum && k > 0) {
+                myStack.pop();
+                k--;
+            }
+
+            // push the current number in num on stack
+            myStack.push(charInNum);
+        }
+
+        // check if k > 0
+        while (k > 0 && !myStack.empty()) {
+            myStack.pop();
+            k--;
+        }
+
+        // convert stack to a string
+        String resStr = stackToStr(myStack);
+
+        // edge case when all nums are removed
+        if (resStr.length() == 0) return "0";
+
+        return resStr;
+    }
+}
+
+// Utilize a monotonic stack to keep the numbers in order while iterating the num string:
+    // - push the number on to the stack
+    // - if the next number is greater than the top number in stack, keep pushing
+    // - else, pop the top number and replace it with this new number
+    // the max we can replace is k?
+    // convert stack to string and create the 
