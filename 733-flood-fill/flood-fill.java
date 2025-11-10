51275a7e6708502bb61810e9449a06c72b1f7b50
pmn48 <138720690+pmn48@users.noreply.github.com>
2025-11-10 16:17:19 -0500

    Time: 1 ms (42.98%) | Memory: 46.7 MB (11.55%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -11,12 +11,19 @@ class Solution {
         int m = image.length;
         int n = image[0].length;
 
-        // initialize visited array and queue for neighbors
-        boolean[][] visited = new boolean[m][n]; // default value for all is false
+        // run BFS to process qualifying neighbors
+        bfs(image, sr, sc, m, n, ogColor, color);     
+        return image;
+    }
+
+
+    /**
+    Helper function for BFS: neighbors that have the same color as starting pixel
+    will be processed into queue */
+    private void bfs(int[][] image, int sr, int sc, int rowDim, int colDim, int ogColor, int color) {
         Queue<int[]> queue = new LinkedList<>(); // neighbors that have same color as starting pixel and need to be changed
 
-        // add starting pixel to visited and queue
-        visited[sr][sc] = true;
+        // add starting pixel queue
         queue.add(new int[] {sr,sc});
 
         // process the queue to find qualifying neighbors
@@ -32,25 +39,20 @@ class Solution {
                 int curCol = col + NCOL[i];
 
                 // only process in-bound and not visited pixels
-                if (isValidPixel(curRow, curCol, m, n) && !visited[curRow][curCol]) {
-                    visited[curRow][curCol] = true;
-
-                    // pixels added to queue only when they have the same color as the starting pixel
-                    if (image[curRow][curCol] == ogColor) {
-                        image[curRow][curCol] = color;
-                        queue.add(new int[] {curRow,curCol});
-                    }
+                // only visit ones that have the original color
+                if (isValidPixel(curRow, curCol, rowDim, colDim) && image[curRow][curCol] == ogColor) {
+                    image[curRow][curCol] = color; // if visited, change color
+                    queue.add(new int[] {curRow,curCol}); // pixels added to queue only when they have the same color as the starting pixel
                 }
             }
         }
-        return image;
     }
 
+    /**
+    Helper function to check if pixel is in bound */
     public static boolean isValidPixel(int row, int col, int row_bound, int col_bound) {
         return (row >= 0 && row < row_bound && col >= 0 && col < col_bound);
     }
-
-
 }
 // From the starting pixel, run BFS to find its neighbors (shares same sides) - in queue
 // For each neighbor:
